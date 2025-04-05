5d39e48ed3f92426453ef42293c659fc2dd3ade9
bassamshuli <salimsh909@gmail.com>
2025-04-05 19:18:03 +0200

    fds
    sdf

---
+++ Source/Test/Private/IntroGameMode.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "IntroGameMode.h"
+#include "Blueprint/UserWidget.h"
+
+
+AIntroGameMode::AIntroGameMode(const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer)
+{
+    static ConstructorHelpers::FClassFinder<UUserWidget> WidgetClass(TEXT("/Game/BluePrints/WBP_StartMenu")); // <-- path corretto!
+    if (WidgetClass.Succeeded())
+    {
+        StartMenuWidgetClass = WidgetClass.Class;
+    }
+}
+
+void AIntroGameMode::BeginPlay()
+{
+    Super::BeginPlay();
+    UE_LOG(LogTemp, Warning, TEXT("IntroGameMode: BeginPlay() called"));
+
+    if (StartMenuWidgetClass)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("IntroGameMode: Creating Widget..."));
+
+        UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), StartMenuWidgetClass);
+        if (Widget)
+        {
+            Widget->AddToViewport();
+            UE_LOG(LogTemp, Warning, TEXT("IntroGameMode: Widget Added!"));
+        }
+        else
+        {
+            UE_LOG(LogTemp, Error, TEXT("IntroGameMode: Widget creation failed"));
+        }
+    }
+    else
+    {
+        UE_LOG(LogTemp, Error, TEXT("IntroGameMode: StartMenuWidgetClass is NULL"));
+    }
+}
+
+
---
+++ Source/Test/Private/IntroSceneActor.cpp
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "IntroSceneActor.h"
+#include "Components/StaticMeshComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/DirectionalLightComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Materials/MaterialInterface.h"
+
+AIntroSceneActor::AIntroSceneActor()
+{
+    PrimaryActorTick.bCanEverTick = false;
+
+    RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+
+    // === PLANE (con immagine di sfondo) ===
+    Plane = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Plane"));
+    Plane->SetupAttachment(RootComponent);
+
+    static ConstructorHelpers::FObjectFinder<UStaticMesh> PlaneMesh(TEXT("/Engine/BasicShapes/Plane"));
+    if (PlaneMesh.Succeeded())
+    {
+        Plane->SetStaticMesh(PlaneMesh.Object);
+        Plane->SetRelativeScale3D(FVector(12.0f, 9.0f, 1.0f)); // proporzioni 16:9
+        Plane->SetRelativeRotation(FRotator(0.0f, 90.0f, 0.0f)); // ruota per adattarsi alla camera
+    }
+
+    static ConstructorHelpers::FObjectFinder<UMaterialInterface> BackgroundMaterial(TEXT("/Game/Materials/M_IntroBackground")); // cambia il path se diverso
+    if (BackgroundMaterial.Succeeded())
+    {
+        Plane->SetMaterial(0, BackgroundMaterial.Object);
+    }
+
+    // === CAMERA ===
+    Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+    Camera->SetupAttachment(RootComponent);
+    Camera->SetRelativeLocation(FVector(0.0f, 0.0f, 800.0f));      // sopra il piano
+    Camera->SetRelativeRotation(FRotator(-90.0f, 0.0f, 0.0f));     // guarda in basso
+
+    // === LUCE ===
+    DirectionalLight = CreateDefaultSubobject<UDirectionalLightComponent>(TEXT("DirectionalLight"));
+    DirectionalLight->SetupAttachment(RootComponent);
+    DirectionalLight->SetRelativeRotation(FRotator(-45.0f, 0.0f, 0.0f));
+}
+
+void AIntroSceneActor::BeginPlay()
+{
+    Super::BeginPlay();
+
+    if (Camera)
+    {
+        Camera->Activate();  // Forza l'attivazione della camera
+    }
+
+    APlayerController* PC = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+    if (PC && Camera)
+    {
+        PC->SetViewTargetWithBlend(this, 0.0f);
+    }
+}
+
